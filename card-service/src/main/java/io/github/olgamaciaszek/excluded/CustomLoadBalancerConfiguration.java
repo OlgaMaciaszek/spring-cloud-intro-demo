4538582d3a88500d0b90acb2c5aea4ec349c6a6a
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2020-08-12 11:27:39 +0200

    Upgrade. Add config server demo.

--- card-service/src/main/java/io/github/olgamaciaszek/excluded/CustomLoadBalancerConfiguration.java
+++ card-service/src/main/java/io/github/olgamaciaszek/excluded/CustomLoadBalancerConfiguration.java
@@ -4,7 +4,7 @@ import io.github.olgamaciaszek.cardservice.config.TestRoundRobinLoadBalancer;
 
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
-import org.springframework.cloud.loadbalancer.core.ServiceInstanceSupplier;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
@@ -19,6 +19,6 @@ public class CustomLoadBalancerConfiguration {
 			LoadBalancerClientFactory loadBalancerClientFactory) {
 		String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
 		return new TestRoundRobinLoadBalancer(name, loadBalancerClientFactory
-				.getLazyProvider(name, ServiceInstanceSupplier.class));
+				.getLazyProvider(name, ServiceInstanceListSupplier.class));
 	}
 }
