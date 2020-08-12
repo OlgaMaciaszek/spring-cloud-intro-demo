f10f109a7594cf830a67fcb93e4e0b868d047c5b
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2020-08-12 13:45:32 +0200

    Remove CustomRibbonConfiguration. Remove unused imports.

--- card-service/src/main/java/io/github/olgamaciaszek/excluded/CustomLoadBalancerConfiguration.java
+++ card-service/src/main/java/io/github/olgamaciaszek/excluded/CustomLoadBalancerConfiguration.java
@@ -5,7 +5,6 @@ import io.github.olgamaciaszek.cardservice.config.TestRoundRobinLoadBalancer;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
-import org.springframework.cloud.loadbalancer.core.ServiceInstanceSupplier;
 import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
